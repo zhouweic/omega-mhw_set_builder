5b82cf6d580d04047b0a2af877cc5cd2cedb9dcd
WzyJerry <wzy950507@163.com>
2018-09-20 20:28:54 +0800

    完成护石、珠子、部分完成基本配装

--- src/components/standard-set-builder/utils/data-builder.js
+++ src/components/standard-set-builder/utils/data-builder.js
@@ -1,8 +1,8 @@
-import data from '@/assets/database/mhw-omega.json'
+import database from '@/assets/database/mhw-omega.json'
 
 function makeArmor () {
   const armors = []
-  data.armors.forEach(armorGroup => {
+  database.armors.forEach(armorGroup => {
     armorGroup.contains.forEach(item => {
       const jsonItem = JSON.stringify(item)
       const armor = JSON.parse(jsonItem)
@@ -17,4 +17,42 @@ function makeArmor () {
   return armors
 }
 
-export default makeArmor
+function getSkillMaxLevel (skill) {
+  let maxLevel = 0
+  database.skills.forEach(item => {
+    if (item.name === skill) {
+      maxLevel = item.maxLevel
+    }
+  })
+  return maxLevel
+}
+
+function makeJewel (slot) {
+  const jewels = []
+  database.jewels.forEach(item => {
+    if (item.slot <= slot) {
+      const jsonItem = JSON.stringify(item)
+      const jewel = JSON.parse(jsonItem)
+      jewel.name = `${jewel.name}【${jewel.slot}】（${jewel.skill}）`
+      jewel.maxLevel = getSkillMaxLevel(jewel.skill)
+      jewels.push(jewel)
+    }
+  })
+  return jewels
+}
+
+function makeCharm () {
+  const charms = []
+  database.charms.forEach(item => {
+    const jsonItem = JSON.stringify(item)
+    const charm = JSON.parse(jsonItem)
+    charm.name = `${charm.name}（${charm.skills[0].name}${charm.skills[1] ? `&${charm.skills[1].name}` : ''}）`
+    charm.skills.forEach(skill => {
+      skill.maxLevel = getSkillMaxLevel(skill.name)
+    })
+    charms.push(charm)
+  })
+  return charms
+}
+
+export { makeArmor, makeJewel, makeCharm, getSkillMaxLevel }
